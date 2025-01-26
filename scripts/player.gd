9d9d39d116bb69b34343f2c8817fd979e9265d65
Ibrahim Iftikhar <iftikhar.ibs@gmail.com>
2025-01-26 11:56:55 -0500

    Player movement cont.
    Added directional movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var input = Input.get_action_strength("ui_up")
-	apply_central_force(input*Vector3.FORWARD*1200*delta) #make constant value a variable
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+
+	apply_central_force(input * 1200.0 * delta) #make constant value a variable
 	
