1aea65d95d6d23f0d561a81d7428ed3e841619e4
Ethan Wellenreiter <ewellenreiter@gmail.com>
2025-01-26 12:49:21 -0500

    idk. basic player movement
    Signed-off-by: Ethan Wellenreiter <ewellenreiter@gmail.com>

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,6 +4,29 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var mouse_sensitivity_y : float = 0.002
+var mouse_sensitivity_x : float = 0.002
+var mouse_locked : bool = false
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	mouse_locked = true
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode >= Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity_y)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity_x)
+		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+
+		
+	if event.is_action_released("escape"):
+		if mouse_locked:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			mouse_locked = false
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			mouse_locked = true
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +34,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -26,3 +49,7 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+func _process(delta:float) -> void:
+	pass
