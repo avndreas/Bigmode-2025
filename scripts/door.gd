81ba776cbe46dbf79a7eb3ee9eee0ce991169028
Ethan Wellenreiter <ewellenreiter@gmail.com>
2025-01-27 15:24:58 -0500

    Adding doors/fixing/making a template sensor door
    Signed-off-by: Ethan Wellenreiter <ewellenreiter@gmail.com>

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,26 +1,44 @@
 extends Activator
 class_name Door
 
-@export var door_body : AnimatableBody3D = get_parent()
+@export var door_body : AnimatableBody3D
 
 @export var movement_speed : float = 5
-@export var pos2 : Vector3 = Vector3.UP
+@export var opened_pos : Vector3 = Vector3.UP
 
-@onready var pos1 : Vector3 = door_body.position
-@onready var current_goal_pos : Vector3 = pos1
+@onready var closed_pos : Vector3 = Vector3.ZERO
+@onready var current_goal_pos : Vector3 = closed_pos
 
 var stop : bool = true
 
 ## Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
-	#pass # Replace with function body.
-#
-#
+	#if not door_body == null:
+		#for child in get_children():
+			#print("child")
+			#if child is AnimatableBody3D:
+				#door_body = child
+				##print("hi1")
+				#break
+
+
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 	#pass
 	
 func _physics_process(delta: float) -> void:
+	#print("hi2")
+	if door_body == null:
+		#print("ddasdasd")
+		for child in get_children():
+			#print("child")
+			if child is AnimatableBody3D:
+				door_body = child
+				closed_pos = door_body.position
+				#print("hi1")
+				break
+		return
+		
 	if door_body.position != current_goal_pos and not stop:
 		#print("door_moving")
 		var move_dir : Vector3 = (current_goal_pos - door_body.position).normalized()
@@ -35,12 +53,24 @@ func _physics_process(delta: float) -> void:
 
 func activate() -> void:
 	#print("hi")
-	if current_goal_pos == pos1:
+	if current_goal_pos == opened_pos:
 		#print("moving_to_pos2")
-		current_goal_pos = pos2
+		current_goal_pos = closed_pos
 		stop = false
 	else:
 		#print("moving_to_pos1")
-		current_goal_pos = pos1
+		current_goal_pos = opened_pos
+		stop = false
+		
+func close(body : Node3D = null) -> void:
+	#print("close")
+	if body == null or body is CharacterBody3D:
+		current_goal_pos = closed_pos
+		stop = false
+	
+func open(body : Node3D = null) -> void:
+	#print("open")
+	if body == null or body is CharacterBody3D:
+		current_goal_pos = opened_pos
 		stop = false
 	
