d2972651fca9fff1e15e391656c67a40368b9847
Ethan Wellenreiter <ewellenreiter@gmail.com>
2025-01-27 15:25:37 -0500

    Resolution/viewport indifferent mouse sensitivity
    Signed-off-by: Ethan Wellenreiter <ewellenreiter@gmail.com>

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,8 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-var mouse_sensitivity_y : float = 0.002
-var mouse_sensitivity_x : float = 0.002
+var mouse_sensitivity_y : float = 6
+var mouse_sensitivity_x : float = 6
 var mouse_locked : bool = false
 
 func _ready():
@@ -14,8 +14,8 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode >= Input.MOUSE_MODE_CAPTURED:
-		rotate_y(-event.relative.x * mouse_sensitivity_y)
-		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity_x)
+		rotate_y(-event.relative.x * mouse_sensitivity_y / get_viewport().size.x)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity_x / get_viewport().size.y)
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(89), deg_to_rad(89)) # has the bounds on the up and down looking
 
 		
