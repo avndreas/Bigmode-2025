1a931f248a7fcac48ba2ab6c718922193849341a
epochman <andreas.vinod7474@gmail.com>
2025-01-28 21:56:24 -0500

    Improved jumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,20 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+#const SPEED = 5.0
+#const JUMP_VELOCITY = 4.5
 
 var mouse_sensitivity_y : float = 6
 var mouse_sensitivity_x : float = 6
 var mouse_locked : bool = false
+
+const SPEED = 5.0
+const SPRINT_SPEED = 8.0
+const JUMP_VELOCITY = 6.0
+const SENSITIVITY = 0.003
+
+var gravity = 9.8 * 4
+var speed = SPEED
 
 
 func _input(event):
@@ -18,24 +26,38 @@ func _input(event):
 
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y -= gravity * delta
 
-	# Handle jump.
+	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		gravity = 9.8 * 2
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_released("jump"):
+		gravity = 9.8 * 4
+		
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = 0.0
+			velocity.z = 0.0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 4.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 4.0)
 
 	move_and_slide()
 
