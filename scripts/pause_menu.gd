7ce9b66407db23fc2fee30b1672ce6dfb1fac7d0
Ethan Wellenreiter <ewellenreiter@gmail.com>
2025-01-30 11:59:40 -0500

    Adding restart button for level
    Signed-off-by: Ethan Wellenreiter <ewellenreiter@gmail.com>

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -5,14 +5,14 @@ func _ready() -> void:
 	$AnimationPlayer.play("RESET")
 
 func resume():
-	print("resuming")
+	#print("resuming")
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
 	Universe.in_game = true
 	visible = false
 	
 func pause():
-	print("pausing")
+	#print("pausing")
 	get_tree().paused = true
 	Universe.in_game = false
 	visible = true
@@ -29,14 +29,16 @@ func testEsc():
 
 	
 func _on_resume_pressed() -> void:
-	print("hi")
+	#print("hi")
 	resume()
 
 func _on_restart_pressed():
+	resume()
 	get_tree().reload_current_scene()
+	Universe.in_game = true
 
 func _on_quit_pressed():
 	get_tree().quit()
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	testEsc()
