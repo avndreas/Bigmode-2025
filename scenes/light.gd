d730b06748c53eaf8705e167710d8aa1c55f15ac
avndreas <andreas.vinod7474@gmail.com>
2025-01-31 22:21:38 -0500

    Minor tweaks

--- scenes/light.gd
+++ scenes/light.gd
@@ -5,22 +5,23 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	light.visible = true
+	#light.visible = true
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func toggleLight() -> bool:
-	if light.visible:
-		light.visible = false
-	else:
-		light.visible = true
-	return light.visible
-
-func getLightStatus() -> bool:
-	return light.visible
-
-func setLightStatus(lightStatus: bool):
-	light.visible = lightStatus
+#func toggleLight() -> bool:
+	#if light.visible:
+		#light.visible = false
+	#else:
+		#light.visible = true
+	#return light.visible
+#
+#func getLightStatus() -> bool:
+	#return light.visible
+#
+#func setLightStatus(lightStatus: bool):
+	#light.visible = lightStatus
